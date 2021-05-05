8af8f9194af3219904b662878993b00c8ff9c1bd
Brett Minnie <github@bdb-studios.co.uk>
2021-05-05 15:31:16 +0100

    Assign!

--- scripts/setup-e2e-tests.sh
+++ scripts/setup-e2e-tests.sh
@@ -49,7 +49,7 @@ then
 fi
 
 AWS_CLI_PATH="$(which aws)"
-$PNC_ELB_NAME=$(echo "cjse-${WORKSPACE}-bichard-7-${WORKSPACE}" | cut -c1-31)
+PNC_ELB_NAME=$(echo "cjse-${WORKSPACE}-bichard-7-${WORKSPACE}" | cut -c1-32)
 aws_credential_check
 WAS_IP=$($AWS_CLI_PATH elbv2 describe-load-balancers --names ${$PNC_ELB_NAME} --query 'LoadBalancers[0].DNSName' --output text)
 PNC_HOST=$($AWS_CLI_PATH elbv2 describe-load-balancers --names cjse-${WORKSPACE}-bichard-7-${WORKSPACE} --query 'LoadBalancers[0].DNSName' --output text)
