cd04d0c41225745fa83a569581d88d3bf2baaf31
Emad Karamad <emad.karamad@madetech.com>
2021-05-21 09:41:50 +0100

    Add StepFunctionsHelper

---
+++ helpers/StepFunctionsHelper.js
@@ -0,0 +1,39 @@
+const { StepFunctions } = require("aws-sdk");
+const uuid = require("uuid").v4;
+
+class StepFunctionsHelper {
+  constructor(config) {
+    this.stateMachine = new StepFunctions({
+      endpoint: config.url,
+      region: config.region,
+      credentials: {
+        accessKeyId: "test",
+        secretAccessKey: "test"
+      }
+    });
+    this.region = config.region;
+    this.incomingMessageBucketName = config.incomingMessageBucketName;
+  }
+
+  async runIncomingMessagesStateMachine(s3FileName) {
+    const executionName = s3FileName.replace(/\//g, "_") + uuid();
+    this.stateMachine
+      .startExecution({
+        stateMachineArn: `arn:aws:states:${this.region}:000000000000:stateMachine:IncomingMessageHandler`,
+        input: `{
+          "detail": {
+            "requestParameters": {
+              "bucketName": "${this.incomingMessageBucketName}",
+              "key": "${s3FileName}"
+            }
+          }
+        }`,
+        name: executionName
+      })
+      .promise()
+      .then(() => undefined)
+      .catch((error) => error);
+  }
+}
+
+module.exports = StepFunctionsHelper;
