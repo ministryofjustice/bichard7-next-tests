72d9dc99e95c39d71827d402addc770dd96d5d8f
Emad Karamad <emad.karamad@madetech.com>
2021-05-26 18:05:49 +0100

    Update uploadToS3 function to check for errors

--- steps/message.js
+++ steps/message.js
@@ -1,37 +1,34 @@
 const uuid = require("uuid").v4;
+const isError = require("../utils/isError");
 
 const uploadToS3 = (context) => async (messageId, externalCorrelationId, messageReceivedDate) => {
   const fileName = await context.incomingMessageBucket.upload(messageId, externalCorrelationId, messageReceivedDate);
 
-  if (typeof fileName !== "string") {
-    return false;
+  if (isError(fileName)) {
+    throw fileName;
   }
 
   if (context.isLocalWorkspace) {
     const stateMachineResult = await context.incomingMessageHandlerStateMachine.execute(fileName);
 
-    if (stateMachineResult) {
-      return false;
+    if (isError(stateMachineResult)) {
+      throw stateMachineResult;
     }
   }
-
-  return true;
 };
 
 const sendMessage = (context) => async (messageId, externalCorrelationId = uuid(), date = new Date()) => {
   const messageIdVal = messageId || "court_result_input_1";
 
   if (context.shouldUploadMessagesToS3) {
-    uploadToS3(context)(messageId, externalCorrelationId, date);
+    await uploadToS3(context)(messageId, externalCorrelationId, date);
   } else {
     await context.mq.sendMessage("COURT_RESULT_INPUT_QUEUE", messageIdVal);
   }
 };
 
 const aMessageIsReceived = (step) => {
-  step("a message is received", async () => {
-    await sendMessage("court_result_input_1");
-  });
+  step("a message is received", () => sendMessage("court_result_input_1"));
 };
 
 module.exports = { sendMessage, uploadToS3, aMessageIsReceived };
