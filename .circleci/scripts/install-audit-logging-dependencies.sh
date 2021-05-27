4757d0ceb86967e9099bea603d6c2b243169a2b5
Emad Karamad <emad.karamad@madetech.com>
2021-05-27 16:17:16 +0100

    Update CircleCi config to spin up audit-logging components

---
+++ .circleci/scripts/install-audit-logging-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~/bichard7-next-services
+
+# Installing Nodejs version in .nvmrc
+nvm install
+
+# Install awslocal
+pip3 install awscli-local
+
+# Configure aws
+aws configure set aws_access_key_id test
+aws configure set aws_secret_access_key test
+aws configure set default.region us-east-1
+
+cd -
