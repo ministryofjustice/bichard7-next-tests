b9531d7ef26f4cb509b2901c3a6262dec8249415
Emad Karamad <emad.karamad@madetech.com>
2021-05-27 20:16:35 +0100

    Move default values to defaults.js

---
+++ utils/defaults.js
@@ -0,0 +1,24 @@
+const defaults = {
+  awsUrl: "http://localhost:4566",
+  awsRegion: "us-east-1",
+  incomingMessageBucket: "incoming-messages",
+  postgresHost: "localhost",
+  postgresPort: 5432,
+  postgresUser: "bichard",
+  postgresPassword: "password",
+  mqUrl: "failover:(stomp://localhost:61613)",
+  mqUser: "admin",
+  mqPassword: "admin",
+  db2Host: "localhost",
+  db2Port: 50000,
+  db2User: "db2inst1",
+  db2Password: "TEST_DB2INST1_PASSWORD",
+  ibmMqUrl: "localhost:10443",
+  ibmMqUser: "app",
+  ibmMqPassword: "passw0rd",
+  ibmMqQmgr: "BR7_QM",
+  pncHost: "localhost",
+  pncPort: "3000"
+};
+
+module.exports = defaults;
--- utils/helpers.js
+++ utils/helpers.js
@@ -6,85 +6,65 @@ const MockPNCHelper = require("../helpers/MockPNCHelper");
 const IncomingMessageBucket = require("../helpers/IncomingMessageBucket");
 const IncomingMessageHandlerStateMachine = require("../helpers/IncomingMessageHandlerStateMachine");
 const AuditLoggingApi = require("../helpers/AuditLoggingApi");
+const defaults = require("./defaults");
 
 class Bichard {
   constructor() {
-    const defaultAwsUrl = "http://localhost:4566";
-    const defaultAwsRegion = "us-east-1";
-    const defaultIncomingMessageBucket = "incoming-messages";
-    const defaultPostgresHost = "localhost";
-    const defaultPostgresPort = 5432;
-    const defaultPostgresUser = "bichard";
-    const defaultPostgresPassword = "password";
-    const defaultMqUrl = "failover:(stomp://localhost:61613)";
-    const defaultMqUser = "admin";
-    const defaultMqPassword = "admin";
-    const defaultDb2Host = "localhost";
-    const defaultDb2Port = 50000;
-    const defaultDb2User = "db2inst1";
-    const defaultDb2Password = "TEST_DB2INST1_PASSWORD";
-    const defaultIbmMqUrl = "localhost:10443";
-    const defaultIbmMqUser = "app";
-    const defaultIbmMqPassword = "passw0rd";
-    const defaultIbmMqQmgr = "BR7_QM";
-    const defaultPncHost = "localhost";
-    const defaultPncPort = "3000";
-
     const stackType = process.env.STACK_TYPE || "next";
     this.isLocalWorkspace = process.env.WORKSPACE === "local-next" || process.env.WORKSPACE === "local-baseline";
     this.shouldUploadMessagesToS3 = (process.env.MESSAGE_ENTRY_POINT || "mq") === "s3";
 
     if (stackType === "next") {
       this.db = new PostgresHelper({
-        host: process.env.DB_HOST || defaultPostgresHost,
-        port: process.env.DB_PORT || defaultPostgresPort,
+        host: process.env.DB_HOST || defaults.postgresHost,
+        port: process.env.DB_PORT || defaults.postgresPort,
         database: "bichard",
-        user: process.env.DB_USER || defaultPostgresUser,
-        password: process.env.DB_PASSWORD || defaultPostgresPassword
+        user: process.env.DB_USER || defaults.postgresUser,
+        password: process.env.DB_PASSWORD || defaults.postgresPassword
       });
 
       this.mq = new ActiveMqHelper({
-        url: process.env.MQ_URL || defaultMqUrl,
-        login: process.env.MQ_USER || defaultMqUser,
-        password: process.env.MQ_PASSWORD || defaultMqPassword
+        url: process.env.MQ_URL || defaults.mqUrl,
+        login: process.env.MQ_USER || defaults.mqUser,
+        password: process.env.MQ_PASSWORD || defaults.mqPassword
       });
 
       this.incomingMessageBucket = new IncomingMessageBucket({
-        url: process.env.AWS_URL || defaultAwsUrl,
-        region: process.env.S3_REGION || defaultAwsRegion,
-        incomingMessageBucketName: process.env.S3_INCOMING_MESSAGE_BUCKET || defaultIncomingMessageBucket
+        url: process.env.AWS_URL || defaults.awsUrl,
+        region: process.env.S3_REGION || defaults.awsRegion,
+        incomingMessageBucketName: process.env.S3_INCOMING_MESSAGE_BUCKET || defaults.incomingMessageBucket
       });
 
       this.incomingMessageHandlerStateMachine = new IncomingMessageHandlerStateMachine({
-        url: process.env.AWS_URL || defaultAwsUrl,
-        region: process.env.INCOMING_MESSAGE_HANDLER_REGION || defaultAwsRegion,
-        incomingMessageBucketName: process.env.S3_INCOMING_MESSAGE_BUCKET || defaultIncomingMessageBucket
+        url: process.env.AWS_URL || defaults.awsUrl,
+        region: process.env.INCOMING_MESSAGE_HANDLER_REGION || defaults.awsRegion,
+        incomingMessageBucketName: process.env.S3_INCOMING_MESSAGE_BUCKET || defaults.incomingMessageBucket
       });
 
       this.auditLoggingApi = new AuditLoggingApi({
-        url: process.env.AWS_URL || defaultAwsUrl,
-        region: process.env.AUDIT_LOGGING_API_REGION || defaultAwsRegion
+        url: process.env.AWS_URL || defaults.awsUrl,
+        region: process.env.AUDIT_LOGGING_API_REGION || defaults.awsRegion
       });
     } else if (stackType === "baseline") {
       this.db = new Db2Helper({
-        host: process.env.DB_HOST || defaultDb2Host,
-        port: process.env.DB_PORT || defaultDb2Port,
+        host: process.env.DB_HOST || defaults.db2Host,
+        port: process.env.DB_PORT || defaults.db2Port,
         database: "br7own",
-        user: process.env.DB_USER || defaultDb2User,
-        password: process.env.DB_PASSWORD || defaultDb2Password
+        user: process.env.DB_USER || defaults.db2User,
+        password: process.env.DB_PASSWORD || defaults.db2Password
       });
 
       this.mq = new IbmMqHelper({
-        url: process.env.MQ_URL || defaultIbmMqUrl,
-        user: process.env.MQ_USER || defaultIbmMqUser,
-        password: process.env.MQ_PASSWORD || defaultIbmMqPassword,
-        queueManager: process.env.MQ_QMGR || defaultIbmMqQmgr
+        url: process.env.MQ_URL || defaults.ibmMqUrl,
+        user: process.env.MQ_USER || defaults.ibmMqUser,
+        password: process.env.MQ_PASSWORD || defaults.ibmMqPassword,
+        queueManager: process.env.MQ_QMGR || defaults.ibmMqQmgr
       });
     }
 
     this.pnc = new MockPNCHelper({
-      host: process.env.PNC_HOST || defaultPncHost,
-      port: process.env.PNC_PORT || defaultPncPort
+      host: process.env.PNC_HOST || defaults.pncHost,
+      port: process.env.PNC_PORT || defaults.pncPort
     });
   }
 }
