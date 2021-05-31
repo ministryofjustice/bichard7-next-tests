ab1ca3d05909ea4f6455d2a95642d8f48fae4955
Emad Karamad <emad.karamad@madetech.com>
2021-05-31 15:56:35 +0100

    Fix resolving issue with Poller

--- utils/Poller.js
+++ utils/Poller.js
@@ -7,21 +7,30 @@ class Poller {
 
   poll(options) {
     const { timeout, delay, condition, name } = options;
+    let isDone = false;
 
     return new Promise((resolve, reject) => {
       const processHandles = [];
 
       const timeoutHandle = setTimeout(() => {
+        if (isDone) {
+          return;
+        }
+        isDone = true;
         clearTimeouts([timeoutHandle, ...processHandles]);
         reject(new Error(`Failed polling due to exceeding the timeout. (${name})`));
       }, timeout);
 
       const process = async () => {
         const result = await this.action();
+        if (isDone) {
+          return;
+        }
         if (condition(result)) {
+          isDone = true;
           clearTimeouts([timeoutHandle, ...processHandles]);
           resolve(result);
-        } else {
+        } else if (!isDone) {
           processHandles.push(setTimeout(process, delay));
         }
       };
