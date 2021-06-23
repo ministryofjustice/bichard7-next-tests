9c8eaf48919747fa13be412e9996ca07f93c9824
Ben Pirt <ben@pirt.co.uk>
2021-06-23 15:58:59 +0100

    Add QS test 107

---
+++ fixtures/pncMocks/q-solution_test_107.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    matchRegex: "CXE01",
+    response: `<?XML VERSION="1.0" STANDALONE="YES"?>
+    <CXE01>
+      <GMH>073ENQR000020SENQASIPNCA05A73000017300000120210316152773000001                                             050001772</GMH>
+      <ASI>
+        <FSC>K04CA</FSC>
+        <IDS>K00/410846F OFFENCE                 </IDS>
+        <CCR>K97/1626/8395Q                 </CCR>
+        <COF>K001    12:15:24:1   TH68006 281120102220081220102220</COF>
+        <COF>K002    12:15:24:1   TH68151 281120102220081220102220</COF>
+        <COF>K003    12:15:24:1   RT88191 281120102220081220102220</COF>
+      </ASI>
+      <GMT>000008073ENQR004540S</GMT>
+    </CXE01>`,
+    expectedRequest: ""
+  },
+  {
+    matchRegex: "CXU02",
+    response: `<?XML VERSION="1.0" STANDALONE="YES"?>
+    <CXU02>
+      <GMH>073GENL000001RNEWREMPNCA05A73000017300000120210415154673000001                                             090001753</GMH>
+      <TXT>A0031-REMAND REPORT HAS BEEN PROCESSED SUCCESSFULLY - ID: 00/263503N </TXT>
+      <GMT>000003073GENL000001S</GMT>
+    </CXU02>`,
+    expectedRequest:
+      "<FSC>K04YZ</FSC><IDS>K00/410846F OFFENCE                 </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I1892                                                                       OFFENCE/FUZZY                                         260920110000</COU><CCH>K001              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1016                      00                                                                            </DIS><CCH>K002              TH68151 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000100.0000                                                                            </DIS><CCH>K003              RT88191 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000200.0000                                                                            </DIS>"
+  }
+];
