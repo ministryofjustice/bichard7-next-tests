adc8298a32d7e95d870afd5861d6635b62539378
Simon Oldham <simon.oldham@madetech.com>
2021-06-25 12:45:33 +0100

    Added lion's share of 028

--- steps/cucumber-js.steps.js
+++ steps/cucumber-js.steps.js
@@ -23,6 +23,7 @@ const {
   visitTeamPage,
   canSeeReports,
   editTeam,
+  clickButton,
   clickMainTab,
   exceptionIsNotEditable,
   cannotReallocateCase,
@@ -68,6 +69,8 @@ When("I open the record for {string}", openRecordFor);
 
 When("I click the {string} menu button", clickMainTab);
 
+When("I click the {string} button", clickButton);
+
 When("I access the {string} report", accessReport);
 
 When("I download the report", downloadCSV);
@@ -108,6 +111,7 @@ Then("I can correct the exception", exceptionIsEditable);
 
 Then("I cannot correct the exception", exceptionIsNotEditable);
 
+Then("the {string} menu item is not visible", buttonIsNotVisible);
 Then("the {string} menu item is not visible", buttonIsNotVisible);
 
 Then("I can reallocate the case to another force area", reallocateCase);
