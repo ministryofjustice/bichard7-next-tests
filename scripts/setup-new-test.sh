e1973b53cab483de76ba68932a95a9bd4c171196
Ben Pirt <ben@pirt.co.uk>
2021-07-05 16:15:33 +0100

    Fix the setup-new-test script

--- scripts/setup-new-test.sh
+++ scripts/setup-new-test.sh
@@ -6,7 +6,7 @@ echo "Setting up files for test $1"
 
 featureDir="features/$1"
 mkdir features/$1
-mv ./q-solution-import/features/$1.feature $featureDir
+mv ./q-solution-import/features/$1.feature $featureDir/test.feature
 mv ./q-solution-import/messages/$1* $featureDir/input-message.xml
 mv ./q-solution-import/ncm/$1* $featureDir/pnc-data.xml
 mv ./q-solution-import/test-steps/$1.pdf $featureDir/test-steps.pdf
