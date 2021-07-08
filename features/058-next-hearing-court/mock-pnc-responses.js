38f2dd4e83c303ab5ef8ddc09708be43441280ae
Ben Pirt <ben@pirt.co.uk>
2021-07-08 13:43:04 +0100

    Update test 058 and exclude until Bichard is updated

--- features/058-next-hearing-court/mock-pnc-responses.js
+++ features/058-next-hearing-court/mock-pnc-responses.js
@@ -4,6 +4,6 @@ module.exports = (ncm) => [
   mockEnquiryFromNCM(ncm),
   mockUpdate("CXU02", {
     expectedRequest:
-      "<FSC>K05YZ</FSC><IDS>K00/37616T  HEARINGCOURT            </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I5892                                                                       HEARINGCOURT/NEXT                                     260920080000</COU><CCH>K001              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I4004                      00                                                                            </DIS>"
+      "<FSC>K05YZ</FSC><IDS>K00/37616T  HEARINGCOURT            </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I5892                                                                       HEARINGCOURT/NEXT                                     260920080000</COU><CCH>K001              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920080000 </ADJ><DIS>I4004    08102008          00                                                                            </DIS>"
   })
 ];
