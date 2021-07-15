0303ad7d700b33eab919081ef82e6c9b5a15fcc5
Ben Pirt <ben@pirt.co.uk>
2021-07-15 17:50:28 +0100

    Use direct JWT login for the tests

---
+++ utils/dummyUserData.js
@@ -0,0 +1,58 @@
+module.exports = {
+  bichard01: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7Allocator", "B7Audit", "B7ExceptionHandler", "B7GeneralHandler", "B7Supervisor", "B7TriggerHandler"]
+  },
+  allocator1: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7Allocator"]
+  },
+  audit1: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7Audit"]
+  },
+  exceptionhandler1: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7ExceptionHandler"]
+  },
+  generalhandler1: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7GeneralHandler"]
+  },
+  supervisor1: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7Supervisor"]
+  },
+  triggerhandler1: {
+    inclusionList: ["B01", "B41ME00"],
+    groups: ["B7TriggerHandler"]
+  },
+  allocator2: {
+    inclusionList: ["B40ME00"],
+    groups: ["B7Allocator"]
+  },
+  audit2: {
+    inclusionList: ["B40ME00"],
+    groups: ["B7Audit"]
+  },
+  exceptionhandler2: {
+    inclusionList: ["B40ME00"],
+    groups: ["B7ExceptionHandler"]
+  },
+  generalhandler2: {
+    inclusionList: ["B40ME00"],
+    groups: ["B7GeneralHandler"]
+  },
+  supervisor2: {
+    inclusionList: ["B40ME00"],
+    groups: ["B7Supervisor"]
+  },
+  triggerhandler2: {
+    inclusionList: ["B40ME00"],
+    groups: ["B7TriggerHandler"]
+  },
+  nogroupsassigned: {
+    inclusionList: ["B40ME00"],
+    groups: []
+  }
+};
