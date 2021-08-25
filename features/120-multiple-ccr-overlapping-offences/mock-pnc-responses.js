70c409ada162f5d2951140a8b3cacff2d9594b85
Ben Pirt <ben@pirt.co.uk>
2021-08-25 13:55:51 +0100

    Update QS120 with real data from the PNC

--- features/120-multiple-ccr-overlapping-offences/mock-pnc-responses.js
+++ features/120-multiple-ccr-overlapping-offences/mock-pnc-responses.js
@@ -1,6 +1,6 @@
-const { mockUpdate } = require("../../utils/pncMocks");
+const { dummyUpdate } = require("../../utils/pncMocks");
 
-module.exports = (ncm) => [
+module.exports = () => [
   {
     matchRegex: "CXE01",
     response: `<?XML VERSION="1.0" STANDALONE="YES"?>
@@ -8,18 +8,17 @@ module.exports = (ncm) => [
       <GMH>073ENQR000020SENQASIPNCA05A73000017300000120210316152773000001                                             050001772</GMH>
       <ASI>
         <FSC>K04CA</FSC>
-        <IDS>K00/410847G COPPERFIELD             </IDS>
-        <CCR>K97/1626/8395Q                 </CCR>
-        <COF>K001    12:15:24:1   TH68006 281120102220051220102220</COF>
-        <COF>K002    12:15:24:1   TH68151 281120102220051220102220</COF>
-        <COF>K003    12:15:24:1   RT88191 281120102220051220102220</COF>
+        <IDS>K12/14X     AVALON                  </IDS>
+        <CCR>K12/2732/15R                   </CCR>
+        <COF>K001    1:9:7:1      OF61016 01062009                </COF>
+        <CCR>K12/2732/16T                   </CCR>
+        <COF>K001    1:9:7:1      OF61016 01062009                </COF>
+        <CCR>K12/2732/17U                   </CCR>
+        <COF>K001    1:9:7:1      OF61016 01062009                </COF>
       </ASI>
       <GMT>000008073ENQR004540S</GMT>
     </CXE01>`,
     expectedRequest: ""
   },
-  mockUpdate("CXU02", {
-    expectedRequest:
-      "<FSC>K01YZ</FSC><IDS>K00/410846F OFFENCE                 </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I1892                                                                       OFFENCE/FUZZY                                         260920110000</COU><CCH>K001              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1016                      00                                                                            </DIS><CCH>K002              TH68151 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000100.0000                                                                            </DIS><CCH>K003              RT88191 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000200.0000                                                                            </DIS>"
-  })
+  dummyUpdate
 ];
