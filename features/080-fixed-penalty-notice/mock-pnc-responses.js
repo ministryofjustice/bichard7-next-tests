23d51ceab9f1a8ba53b43b4a24cb6d1298175ff7
Ben Pirt <ben@pirt.co.uk>
2021-08-26 11:00:57 +0100

    Update QS080 with PNC data

--- features/080-fixed-penalty-notice/mock-pnc-responses.js
+++ features/080-fixed-penalty-notice/mock-pnc-responses.js
@@ -1,23 +1,24 @@
 const { mockUpdate } = require("../../utils/pncMocks");
 
-module.exports = (ncm) => [
+module.exports = () => [
   {
     matchRegex: "CXE01",
     response: `<?XML VERSION="1.0" STANDALONE="YES"?>
     <CXE01>
       <GMH>073ENQR000020SENQASIPNCA05A73000017300000120210316152773000001                                             050001772</GMH>
       <ASI>
-        <FSC>K04CA</FSC>
-        <IDS>K00/459646G WILLIAMS                </IDS>
-        <CCR>K97/1626/8395Q                 </CCR>
-        <COF>K001    12:15:24:1   CJ67002 010620092220            </COF>
+        <FSC>K01ZD</FSC>
+        <IDS>K13/4B      WILLIAMS                </IDS>
+        <PCR>K01ZD/1234/09       </PCR>
+        <COF>K001    12:21:5:1    CJ67002 01062009                </COF>
+        <DIS>I1109000C 80.00                                                                                          </DIS>
       </ASI>
       <GMT>000008073ENQR004540S</GMT>
     </CXE01>`,
     expectedRequest: ""
   },
-  mockUpdate("CXU02", {
+  mockUpdate("CXU07", {
     expectedRequest:
-      "<FSC>K04YZ</FSC><IDS>K00/459646G WILLIAMS                </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I2732                                                                       WILLIAMS/PETER                                        261120090000</COU><CCH>K001              CJ67002 </CCH><ADJ>INOT GUILTY   GUILTY        261120090000 </ADJ><DIS>I1015            0000100.0000                                                                            </DIS>"
+      "<FSC>K01YZ</FSC><IDS>K13/4B      WILLIAMS                </IDS><SUB>I2732                                                                       26112009P</SUB><PCR>K01ZD/1234/09                      </PCR><CCH>K001              CJ67002 </CCH><ADJ>INOT GUILTY   GUILTY        261120090000 </ADJ><DIS>I1015            0000100.0000                                                                            </DIS>"
   })
 ];
