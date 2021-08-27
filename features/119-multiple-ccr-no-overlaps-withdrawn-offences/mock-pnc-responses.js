598a9f3d91a38df40dcc3f338b5afd2971306a68
Ben Pirt <ben@pirt.co.uk>
2021-08-27 09:59:25 +0100

    Update QS119

--- features/119-multiple-ccr-no-overlaps-withdrawn-offences/mock-pnc-responses.js
+++ features/119-multiple-ccr-no-overlaps-withdrawn-offences/mock-pnc-responses.js
@@ -1,25 +1,72 @@
 const { mockUpdate } = require("../../utils/pncMocks");
 
-module.exports = (ncm) => [
+module.exports = () => [
   {
     matchRegex: "CXE01",
-    response: `<?XML VERSION="1.0" STANDALONE="YES"?>
+    response: `<?xml version="1.0" standalone="yes"?>
     <CXE01>
-      <GMH>073ENQR000020SENQASIPNCA05A73000017300000120210316152773000001                                             050001772</GMH>
+      <GMH>073ENQR000074RENQASIPNCA05A73000017300000120210827074173000001                                             050001861</GMH>
       <ASI>
-        <FSC>K04CA</FSC>
-        <IDS>K00/410847G COPPERFIELD             </IDS>
-        <CCR>K97/1626/8395Q                 </CCR>
-        <COF>K001    12:15:24:1   TH68006 281120102220051220102220</COF>
-        <COF>K002    12:15:24:1   TH68151 281120102220051220102220</COF>
-        <COF>K003    12:15:24:1   RT88191 281120102220051220102220</COF>
+        <FSC>K01VK</FSC>
+        <IDS>K09/478F    NOLAN                   </IDS>
+        <CCR>K09/0428/444E                  </CCR>
+        <COF>K001    1:9:7:1      OF61016 01062009                </COF>
+        <COF>K002    11:6:4:1     PC53001 01062009                </COF>
+        <CCR>K09/0413/445H                  </CCR>
+        <COF>K001    5:5:2:1      TH68001 01062009                </COF>
       </ASI>
-      <GMT>000008073ENQR004540S</GMT>
+      <GMT>000011073ENQR000074R</GMT>
     </CXE01>`,
-    expectedRequest: ""
+    expectedRequest: "",
+    count: 1
   },
   mockUpdate("CXU02", {
     expectedRequest:
-      "<FSC>K01YZ</FSC><IDS>K00/410846F OFFENCE                 </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I1892                                                                       OFFENCE/FUZZY                                         260920110000</COU><CCH>K001              TH68006 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1016                      00                                                                            </DIS><CCH>K002              TH68151 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000100.0000                                                                            </DIS><CCH>K003              RT88191 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000200.0000                                                                            </DIS>"
+      "<FSC>K01YZ</FSC><IDS>K09/478F    NOLAN                   </IDS><CCR>K09/0428/444E                  </CCR><COU>I2732                                                                       NOLAN/NIGEL                                           260920110000</COU><CRT>I2732                                                                       01122011</CRT><CCH>K001              OF61016 </CCH><ADJ>INOT GUILTY   NOT GUILTY    260920110000 </ADJ><DIS>I2006                      00                                                                            </DIS><CCH>K002              PC53001 </CCH><ADJ>INOT GUILTY                         0000 </ADJ><DIS>I2059                      00                                                                            </DIS>",
+    count: 1
+  }),
+  mockUpdate("CXU02", {
+    expectedRequest:
+      "<FSC>K01YZ</FSC><IDS>K09/478F    NOLAN                   </IDS><CCR>K09/0413/445H                  </CCR><COU>I2732                                                                       NOLAN/NIGEL                                           260920110000</COU><CCH>K001              TH68001 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I4011    01122011          00                                                                            </DIS>",
+    count: 1
+  }),
+  mockUpdate("CXU01", {
+    expectedRequest:
+      "<FSC>K01YZ</FSC><IDS>K09/478F    NOLAN                   </IDS><ASR>K09/0000/00/20002F                     </ASR><REM>I26092011B    2732                                                                       011220112732                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      0000                                                                                                                                                                              </REM>",
+    count: 1
+  }),
+  {
+    matchRegex: "CXE01",
+    response: `<?xml version="1.0" standalone="yes"?>
+    <CXE01>
+      <GMH>073ENQR000075RENQASIPNCA05A73000017300000120210827074373000001                                             050001865</GMH>
+      <ASI>
+        <FSC>K01VK</FSC>
+        <IDS>K09/478F    NOLAN                   </IDS>
+        <CCR>K09/0428/444E                  </CCR>
+        <COF>K001    1:9:7:1      OF61016 01062009                </COF>
+        <ADJ>INOT GUILTY   NOT GUILTY    260920110000 </ADJ>
+        <DIS>I2006                                                                                                    </DIS>
+        <CCR>K21/2732/1H                    </CCR>
+        <COF>K001    11:6:4:1     PC53001 01062009                </COF>
+        <CCR>K09/0413/445H                  </CCR>
+        <COF>K001    5:5:2:1      TH68001 01062009                </COF>
+        <ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ>
+        <DIS>I4011    01122011                                                                                        </DIS>
+      </ASI>
+      <GMT>000016073ENQR000075R</GMT>
+    </CXE01>`,
+    expectedRequest: "",
+    count: 1
+  },
+  mockUpdate("CXU02", {
+    expectedRequest:
+      "<FSC>K01YZ</FSC><IDS>K09/478F    NOLAN                   </IDS><CCR>K21/2732/1H                    </CCR><COU>I2732                                                                       NOLAN/NIGEL                                           011220110000</COU><CCH>K001              PC53001 </CCH><ADJ>INOT GUILTY   GUILTY        011220110000 </ADJ><DIS>I1002M10                   00                                                                            </DIS>",
+    count: 1
+  }),
+  mockUpdate("CXU04", {
+    expectedRequest:
+      "<FSC>K01YZ</FSC><IDS>K09/478F    NOLAN                   </IDS><SUB>I2732                                                                       01122011D</SUB><CCR>K09/0413/445H                  </CCR><CCH>K001              TH68001 </CCH><DIS>I1002M10                   00                                                                            </DIS>",
+    count: 1
   })
 ];
