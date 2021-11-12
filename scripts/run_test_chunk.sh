7f1a9cbaa09fa1eb90900b41b6202fad8a91fda7
Emad Karamad <emad.karamad@madetech.com>
2021-11-12 10:25:01 +0000

    Add new line to the end of run_test_chunk.sh

--- scripts/run_test_chunk.sh
+++ scripts/run_test_chunk.sh
@@ -4,4 +4,4 @@ set -e
 
 CHUNKS=$(find features -iname '*.feature' | sort | awk "(NR % $TOTAL_CHUNKS == $CHUNK_NUMBER)" | paste -d ' ' -s -)
 
-./node_modules/.bin/cucumber-js --require steps --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter --tags 'not @Excluded and not @ExcludedOnMaster and not @OnlyRunsOnPNC and not @AuditLog' $CHUNKS 
+./node_modules/.bin/cucumber-js --require steps --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter --tags 'not @Excluded and not @ExcludedOnMaster and not @OnlyRunsOnPNC and not @AuditLog' $CHUNKS
