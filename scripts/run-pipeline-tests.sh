adc4796ed1f205a47a3e4ba865cdfe0ec5ac07ee
Ben Pirt <ben@pirt.co.uk>
2022-01-28 14:13:33 +0000

    Fix the tests script for CD

--- scripts/run-pipeline-tests.sh
+++ scripts/run-pipeline-tests.sh
@@ -2,13 +2,25 @@
 
 set -x
 
-echo "Build was triggered by $TRIGGER"
-
-if [ $TRIGGER = "application-semaphore" ]
+if [ $WORKSPACE = "e2e-test" ]
 then
-  CI=true RECORD=true npm run test
-  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
+  echo "Build was triggered by $TRIGGER"
+  if [ $TRIGGER = "application-semaphore" ]
+  then
+    echo "Running all tests"
+    CI=true RECORD=true npm run test
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
+  else
+    echo "Running must tests"
+    CI=true RECORD=true npm run test:must
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
+  fi
+elif [ $WORKSPACE = "preprod" ]
+then
+  echo "Running preprod tests"
+  CI=true RECORD=true npm run test:preprod
+  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprodauditlogs
 else
-  CI=true RECORD=true npm run test:must
-  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
+  echo "Unknown AWS test workspace"
 fi
+
