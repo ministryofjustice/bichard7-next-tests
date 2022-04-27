d8770654a371e52e0f1c512495113a5484d62dcc
Ben Pirt <ben@pirt.co.uk>
2022-04-27 16:14:25 +0100

    Add functionality to dump comparison logs

--- steps/hooks.js
+++ steps/hooks.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const LogRecorder = require("../helpers/LogRecorder");
 
 const recordLogs = process.env.RECORD === "true" && process.env.RECORD_LOGS === "true";
+const recordComparisons = process.env.RECORD_COMPARISONS === "true";
+const comparisonOutDir = "comparisons";
 let logRecorder;
 
 BeforeAll(async () => {
@@ -29,8 +31,20 @@ AfterAll(() => {
   if (recordLogs) logRecorder.close();
 });
 
+// eslint-disable-next-line consistent-return
 Before(async function (context) {
   this.featureUri = context.gherkinDocument.uri;
+  // eslint-disable-next-line prefer-destructuring
+  this.testId = this.featureUri.match(/features\/([^-]*)-.*/)[1];
+  if (recordComparisons) {
+    if (
+      fs.existsSync(`${comparisonOutDir}/test-${this.testId}.json`) ||
+      fs.existsSync(`${comparisonOutDir}/test-${this.testId}-1.json`)
+    ) {
+      console.log(`Skipping ${this.testId}`);
+      return "pending";
+    }
+  }
   const featureName = this.featureUri.split("/").slice(-2)[0];
   this.outputDir = `./screenshots/${featureName}/${new Date().getTime()}`;
   if (process.env.RECORD === "true") {
@@ -45,7 +59,7 @@ Before(async function (context) {
   }
 });
 
-After(async function () {
+After(async function ({ result: { status } }) {
   await this.browser.close();
   if (process.env.RECORD === "true") {
     if (!this.realPNC) {
@@ -56,5 +70,18 @@ After(async function () {
   }
   if (recordLogs) {
     await logRecorder.save(this);
+  }
+
+  if (recordComparisons && status === "PASSED") {
+    const comparisons = await this.mq.getMessages("PROCESSING_VALIDATION_QUEUE");
+    if (!fs.existsSync(comparisonOutDir)) {
+      fs.mkdirSync(comparisonOutDir);
+    }
+    comparisons.forEach(async (comparison, index) => {
+      const subFile = comparisons.length > 1 ? `-${index}` : "";
+      const outFile = `${comparisonOutDir}/test-${this.testId}${subFile}.json`;
+      const formatted = JSON.stringify(JSON.parse(comparison), null, 2);
+      await fs.promises.writeFile(outFile, formatted);
+    });
   }
 });
