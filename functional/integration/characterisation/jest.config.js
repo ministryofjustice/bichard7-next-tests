d8c0acad07b38b11618773b20b906612ae6e8ae9
Emad Karamad <emad.karamad@madetech.com>
2022-05-26 10:44:37 +0100

    Add jest setup for characterisation tests

---
+++ functional/integration/characterisation/jest.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["node_modules", "build"],
+  testMatch: ["**/*.test.ts"]
+};
