b1c4d3055881d6337653a5e708d14d770ce8fb51
Alice Lee <alice.lee@madetech.com>
2022-05-27 11:42:20 +0100

    Fix defaults module import

--- functional/integration/characterisation/helpers/config.ts
+++ functional/integration/characterisation/helpers/config.ts
@@ -1,4 +1,4 @@
-import defaults from "../../../../utils/defaults"
+import * as defaults from "../../../../utils/defaults"
 
 export default () => ({
   pncHost: process.env.PNC_HOST || defaults.pncHost,
