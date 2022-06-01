4796d6cd8190f7239cc37ea56f375884e5d9b321
Alice Lee <alice.lee@madetech.com>
2022-06-01 09:12:55 +0100

    Enable characterisation tests in codebuild

--- characterisation/jest.config.js
+++ characterisation/jest.config.js
@@ -2,9 +2,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["node_modules", "build"],
-  testMatch: ["**/*.test.ts"],
   // Run these files after jest has been
   // installed in the environment
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"]
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  verbose: true
 };
