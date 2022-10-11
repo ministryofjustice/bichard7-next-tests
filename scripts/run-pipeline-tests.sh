977e7e562d26073a9a4897edd22a8f50233cf72a
csabagyorfi <csaba@madetech.com>
2022-10-11 12:50:00 +0100

    Debug: Run new UI tests only

--- scripts/run-pipeline-tests.sh
+++ scripts/run-pipeline-tests.sh
@@ -8,23 +8,23 @@ then
 
   echo "Running new UI tests"
   CI=true RECORD=true npm run test:nextUI
-  if [ $TRIGGER = "application-semaphore" ]
-  then
-    echo "Running all tests"
-    CI=true RECORD=true npm run test
-    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
-  else
-    echo "Running must tests"
-    CI=true RECORD=true npm run test:must
-    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
-  fi
-  echo "Running characterisation tests"
-  npm run test:characterisation:bichard
-elif [ $WORKSPACE = "preprod" ]
-then
-  echo "Running preprod tests"
-  CI=true RECORD=true npm run test:preprod
-  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprodauditlogs
+  # if [ $TRIGGER = "application-semaphore" ]
+  # then
+  #   echo "Running all tests"
+  #   CI=true RECORD=true npm run test
+  #   CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
+  # else
+  #   echo "Running must tests"
+  #   CI=true RECORD=true npm run test:must
+  #   CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
+  # fi
+  # echo "Running characterisation tests"
+  # npm run test:characterisation:bichard
+# elif [ $WORKSPACE = "preprod" ]
+# then
+#   echo "Running preprod tests"
+#   CI=true RECORD=true npm run test:preprod
+#   CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprodauditlogs
 else
   echo "Unknown AWS test workspace"
 fi
