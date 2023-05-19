978fa7de3f78f7ba446adb94df9a8b4b69ffd1bc
Richard Cane <richard.cane@madetech.com>
2023-05-19 15:40:25 +0100

    Set correct URL for nextui

--- step-definitions/utils/urls.js
+++ step-definitions/utils/urls.js
@@ -2,7 +2,7 @@ const { config } = require("../../utils/config");
 
 const url = (path) => `${config.baseUrl}${path}`;
 
-const caseListPage = () => url("/bichard-ui/InitialRefreshList");
+const caseListPage = () => url("/bichard");
 const authenticateUrl = (token) => url(`/bichard-ui/Authenticate?token=${token}`);
 const logout = () => url("/bichard-ui/bichard-lo");
 const login = () => url("/users/login");
