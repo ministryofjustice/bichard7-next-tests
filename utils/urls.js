5aac84df783c9a7dadacdb30a17917cb892966ff
csabagyorfi <csaba@madetech.com>
2023-05-19 16:32:01 +0100

    Remove redundant process.env.nextUI check from caseListPage url const
    Now we moved this const to a new file for running tests on new UI

--- utils/urls.js
+++ utils/urls.js
@@ -2,7 +2,7 @@ const { config } = require("./config");
 
 const url = (path) => `${config.baseUrl}${path}`;
 
-const caseListPage = () => (process.env.nextUI ? url("/bichard") : url("/bichard-ui/InitialRefreshList"));
+const caseListPage = () => url("/bichard-ui/InitialRefreshList");
 const authenticateUrl = (token) => url(`/bichard-ui/Authenticate?token=${token}`);
 const logout = () => url("/bichard-ui/bichard-lo");
 const login = () => url("/users/login");
