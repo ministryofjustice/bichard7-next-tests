78cb15f9197151ff48ce3ea2b0ba5cea040a67ba
Emad Karamad <emad.karamad@madetech.com>
2023-06-16 09:19:47 +0100

    Add users to NewUI group (#386)

--- utils/dummyUserData.js
+++ utils/dummyUserData.js
@@ -22,7 +22,7 @@ module.exports = {
     visible_courts: ["B01", "B41ME00"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7TriggerHandler"]
+    groups: ["B7TriggerHandler", "B7NewUI"]
   },
   exceptionhandler: {
     inclusionList: ["B01", "B41ME00"],
@@ -38,7 +38,7 @@ module.exports = {
     visible_courts: ["B01", "B41ME00"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7Audit"]
+    groups: ["B7Audit", "B7NewUI"]
   },
   "met.police": {
     inclusionList: ["001"],
@@ -46,7 +46,7 @@ module.exports = {
     visible_courts: ["001"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "wilt.shire": {
     inclusionList: ["054"],
@@ -54,7 +54,7 @@ module.exports = {
     visible_courts: ["054"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "super.fivefour": {
     inclusionList: ["054"],
@@ -62,7 +62,7 @@ module.exports = {
     visible_courts: ["054"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "br7.btp": {
     inclusionList: ["093"],
@@ -70,7 +70,7 @@ module.exports = {
     visible_courts: ["093"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "west.yorkshire": {
     inclusionList: ["013"],
@@ -78,7 +78,7 @@ module.exports = {
     visible_courts: ["013"],
     visible_forces: [],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "essex.user": {
     inclusionList: ["042"],
@@ -86,7 +86,7 @@ module.exports = {
     visible_courts: [],
     visible_forces: ["042"],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "norfolk.user": {
     inclusionList: ["036"],
@@ -94,7 +94,7 @@ module.exports = {
     visible_courts: [],
     visible_forces: ["036"],
     excluded_triggers: [],
-    groups: ["B7Supervisor"]
+    groups: ["B7Supervisor", "B7NewUI"]
   },
   "trigger.fivefourexcl": {
     inclusionList: ["B01HO", "B01EF"],
@@ -102,7 +102,7 @@ module.exports = {
     visible_courts: ["B01HO", "B01EF"],
     visible_forces: [],
     excluded_triggers: ["TRPR0003", "TRPR0004", "TRPR0006"],
-    groups: ["B7TriggerHandler"]
+    groups: ["B7TriggerHandler", "B7NewUI"]
   },
   "general.handlerexclone": {
     inclusionList: ["B01HO", "B01EF"],
@@ -110,7 +110,7 @@ module.exports = {
     visible_courts: ["B01HO", "B01EF"],
     visible_forces: [],
     excluded_triggers: ["TRPR0004", "TRPR0006"],
-    groups: ["B7GeneralHandler"]
+    groups: ["B7GeneralHandler", "B7NewUI"]
   },
   "herts.user": {
     inclusionList: ["041"],
