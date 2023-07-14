1ad840f74c7dcaa2cd6979b8d62c840f9c73e598
Emad Karamad <emad.karamad@madetech.com>
2023-07-14 14:22:21 +0100

    Revert NextUI changes (#411)

--- features/501-trigger-resolved-events/mock-pnc-responses.js
+++
@@ -1,9 +0,0 @@
-const { mockEnquiryFromNCM, mockUpdate } = require("../../utils/pncMocks");
-
-module.exports = (ncm, world = {}) => [
-  mockEnquiryFromNCM(ncm, world),
-  mockUpdate("CXU02", {
-    expectedRequest:
-      "<FSC>K01YZ</FSC><IDS>K00/448754K SEXOFFENCE              </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I2576                                                                       SEXOFFENCE/TRPRFOUR                                   260920110000</COU><CCH>K001              SX03001A</CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I3078                      00                                                                            </DIS><CCH>K002              SX03001 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I3052                      00                                                                            </DIS><CCH>K003              RT88191 </CCH><ADJ>INOT GUILTY   GUILTY        260920110000 </ADJ><DIS>I1015            0000100.0000                                                                            </DIS>"
-  })
-];
--- scripts/run-pipeline-tests.sh
+++ scripts/run-pipeline-tests.sh
@@ -8,34 +8,24 @@ then
 
   if [ $TRIGGER = "application-semaphore" ]
   then
-    echo "Running all tests (old UI)"
+    echo "Running all tests"
     CI=true RECORD=true npm run test
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
-
-    echo "Running all tests (next UI)"
-    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI
-    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:auditlogs
   else
-    echo "Running must tests (old UI)"
+    echo "Running must tests"
     CI=true RECORD=true npm run test:must
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
-
-    echo "Running must tests (next UI)"
-    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:must
-    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:must:auditlogs
   fi
+  # echo "Running new UI tests"
+  # CI=true RECORD=true npm run test:nextUI
 
   echo "Running characterisation tests"
   npm run test:characterisation:bichard
 elif [ $WORKSPACE = "preprod" ]
 then
-  echo "Running preprod tests (old UI)"
+  echo "Running preprod tests"
   CI=true RECORD=true npm run test:preprod
   CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprodauditlogs
-
-  echo "Running preprod tests (next UI)"
-  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:preprod
-  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:preprodauditlogs
 else
   echo "Unknown AWS test workspace"
 fi
