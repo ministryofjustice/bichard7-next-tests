da2b991b6e22599f9908d0fea8fb71a7a11e915c
Emad Karamad <emad.karamad@madetech.com>
2023-07-17 14:11:01 +0100

    Enable Next UI audit log tests in the pipeline
    This reverts commit 1ad840f74c7dcaa2cd6979b8d62c840f9c73e598.

--- scripts/run-pipeline-tests.sh
+++ scripts/run-pipeline-tests.sh
@@ -8,24 +8,34 @@ then
 
   if [ $TRIGGER = "application-semaphore" ]
   then
-    echo "Running all tests"
+    echo "Running all tests (old UI)"
     CI=true RECORD=true npm run test
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
+
+    echo "Running all tests (next UI)"
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:auditlogs
   else
-    echo "Running must tests"
+    echo "Running must tests (old UI)"
     CI=true RECORD=true npm run test:must
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
+
+    echo "Running must tests (next UI)"
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:must
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:must:auditlogs
   fi
-  # echo "Running new UI tests"
-  # CI=true RECORD=true npm run test:nextUI
 
   echo "Running characterisation tests"
   npm run test:characterisation:bichard
 elif [ $WORKSPACE = "preprod" ]
 then
-  echo "Running preprod tests"
+  echo "Running preprod tests (old UI)"
   CI=true RECORD=true npm run test:preprod
   CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprodauditlogs
+
+  echo "Running preprod tests (next UI)"
+  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:preprod
+  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:preprodauditlogs
 else
   echo "Unknown AWS test workspace"
 fi
