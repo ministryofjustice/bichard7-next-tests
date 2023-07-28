8d8a413a471b455f8c6ac8753c4798948a0419b7
Ben Pirt <ben@pirt.co.uk>
2023-07-28 10:36:59 +0100

    Store failed tests in Circle CI

--- scripts/run_test_chunk.sh
+++ scripts/run_test_chunk.sh
@@ -6,4 +6,4 @@ TAGS=$1
 
 CHUNKS=$(find features -iname '*.feature' | sort | awk "(NR % $TOTAL_CHUNKS == $CHUNK_NUMBER)" | paste -d ' ' -s -)
 
-./node_modules/.bin/cucumber-js --require steps --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter --tags "${TAGS}" $CHUNKS
+./node_modules/.bin/cucumber-js --require steps --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter  --format junit:test-results.xml --tags "${TAGS}" $CHUNKS
