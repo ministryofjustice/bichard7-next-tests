7f294ceba3932b7e2a998cf5bf4630e565ae0a45
Richard Cane <richard.cane@madetech.com>
2023-08-08 12:04:13 +0100

    Add log to sendMessage in AMQ helper

--- helpers/ActiveMqHelper.js
+++ helpers/ActiveMqHelper.js
@@ -85,6 +85,7 @@ class ActiveMqHelper {
       destination: `/queue/${queueName}`
     };
 
+    console.log("MQ connected, pushing message to:", queueName);
     return new Promise((resolve, reject) => {
       const writable = client.send(headers);
 
