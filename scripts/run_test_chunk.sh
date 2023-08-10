1703a665e746fe7d2df566b1ff26ab58620eb9a9
Richard Cane <richard.cane@madetech.com>
2023-08-10 19:27:32 +0100

    Add step def for test chunks

--- scripts/run_test_chunk.sh
+++ scripts/run_test_chunk.sh
@@ -3,7 +3,11 @@
 set -e
 
 TAGS=$1
-
 CHUNK=${CHUNK:-$(find features -iname '*.feature' | sort | awk "(NR % $TOTAL_CHUNKS == $CHUNK_NUMBER)" | paste -d ' ' -s -)}
+NEXTUI=${NEXTUI:-"false"}
+
+if [ "$NEXTUI" == "true" ]; then
+  TAGS="${TAGS} and @NextUI"
+fi
 
 ./node_modules/.bin/cucumber-js --require steps --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter  --format junit:test-results.xml --tags "${TAGS}" $CHUNK
