964a20eed8ac208d7431a0c9c5094efee79e5360
Ben Pirt <ben@pirt.co.uk>
2023-09-27 16:39:59 +0100

    Make the CD pipeline always use S3 for tests as MQ will be going away

--- scripts/run-pipeline-tests.sh
+++ scripts/run-pipeline-tests.sh
@@ -9,7 +9,7 @@ then
   if [ $TRIGGER = "application-semaphore" ]
   then
     echo "Running all tests (old UI)"
-    CI=true RECORD=true npm run test
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:auditlogs
 
     echo "Running all tests (next UI)"
@@ -17,7 +17,7 @@ then
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:nextUI:auditlogs
   else
     echo "Running must tests (old UI)"
-    CI=true RECORD=true npm run test:must
+    CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must
     CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:must:auditlogs
 
     echo "Running must tests (next UI)"
@@ -30,7 +30,7 @@ then
 elif [ $WORKSPACE = "preprod" ]
 then
   echo "Running preprod tests (old UI)"
-  CI=true RECORD=true npm run test:preprod
+  CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprod
   CI=true RECORD=true MESSAGE_ENTRY_POINT=s3 npm run test:preprodauditlogs
 
   echo "Running preprod tests (next UI)"
@@ -39,4 +39,3 @@ then
 else
   echo "Unknown AWS test workspace"
 fi
-
