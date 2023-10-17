1ebcdc7a7fddcd227bc323f928b6a9192bb0b5fa
Richard Cane <richard.cane@madetech.com>
2023-10-17 19:04:46 +0100

    Don't need to check existence for value checking

--- steps/steps.js
+++ steps/steps.js
@@ -1,5 +1,5 @@
 const { NEXTUI } = process.env;
-if (NEXTUI && NEXTUI === "true") {
+if (NEXTUI === "true") {
   require("../step-definitions/nextui.steps");
 } else {
   require("../step-definitions/old-cucumber-js.steps");
