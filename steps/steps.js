248d75fa238c0f405d17e3fe136f7ade700d66a5
Richard Cane <richard.cane@madetech.com>
2023-10-17 19:03:55 +0100

    Don't use NEXTUI steps if it's not intended

--- steps/steps.js
+++ steps/steps.js
@@ -1,4 +1,5 @@
-if (process.env.NEXTUI) {
+const { NEXTUI } = process.env;
+if (NEXTUI && NEXTUI === "true") {
   require("../step-definitions/nextui.steps");
 } else {
   require("../step-definitions/old-cucumber-js.steps");
