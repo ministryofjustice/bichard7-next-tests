22359b83a1209013addfba384bc22ac52d275f53
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-10-23 17:59:41 +0100

    Wait for workflow to complete

--- utils/conductor.js
+++ utils/conductor.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const { expect } = require("expect");
+const promisePoller = require("promise-poller").default;
 const { CONDUCTOR_API_URL, CONDUCTOR_API_PASSWORD, CONDUCTOR_API_USER } = require("../helpers/ConductorHelper");
 
 const base64 = (input) => Buffer.from(input).toString("base64");
@@ -11,6 +12,18 @@ const basicAuthenticationHeaders = () => ({
 const conductorApi = axios.create({
   headers: basicAuthenticationHeaders()
 });
+
+const fetchCompletedBichardProcessWorkflow = async (workflowId) => {
+  const workflow = await conductorApi
+    .get(`${CONDUCTOR_API_URL}/api/workflow/${workflowId}`)
+    .then((response) => response.data)
+    .catch((e) => console.log(e));
+
+  if (workflow.status !== "COMPLETED") {
+    throw new Error(`No completed bichard_process workflow with id ${workflowId}`);
+  }
+  return workflow;
+};
 
 const checkConductorWorkflowCompleted = async function (world) {
   const incomingMessageHandlerWorkflowSearch = await conductorApi
@@ -33,10 +46,11 @@ const checkConductorWorkflowCompleted = async function (world) {
     (t) => t.referenceTaskName === "begin_processing"
   );
 
-  const workflow = await conductorApi
-    .get(`${CONDUCTOR_API_URL}/api/workflow/${beginProcessingTask.outputData.workflowId}`)
-    .then((response) => response.data)
-    .catch((e) => console.log(e));
+  const workflow = await promisePoller({
+    taskFn: () => fetchCompletedBichardProcessWorkflow(beginProcessingTask.outputData.workflowId),
+    interval: 100,
+    retries: 900
+  }).catch((e) => e);
 
   expect(workflow.status).toEqual("COMPLETED");
 };
