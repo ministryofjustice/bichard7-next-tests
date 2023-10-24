a92a43db095c86f0b844d98c1e5fd4a9e5d2f448
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-10-24 17:18:01 +0100

    use or not and

--- scripts/run_test_chunk.sh
+++ scripts/run_test_chunk.sh
@@ -14,7 +14,7 @@ fi
 if [ "$LEGACY_PHASE1" == "true" ]; then
   TAGS="${TAGS} and not @CorePhase1"
 elif [ "$LEGACY_PHASE1" == "false" ]; then
-  TAGS="${TAGS} and @CorePhase1"
+  TAGS="${TAGS} or @CorePhase1"
 fi
 
 ./node_modules/.bin/cucumber-js --require steps --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter  --format junit:./cucumber/results/test-results.xml --tags "${TAGS}" $CHUNK
