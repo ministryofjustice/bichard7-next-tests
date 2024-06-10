38ed1166aaa8b339fdca446291cd20100b811b0a
Richard Race <1273965+richrace@users.noreply.github.com>
2024-06-10 15:10:47 +0100

    Make the first HO100332 test pass in legacy Bichard

--- features/717-HO100332/mock-pnc-responses.js
+++ features/717-HO100332/mock-pnc-responses.js
@@ -1,9 +1,23 @@
-// const { mockEnquiryFromNCM, mockUpdate } = require("../../utils/pncMocks");
+const { mockUpdate } = require("../../utils/pncMocks");
 
-// module.exports = (ncm, world = {}) => [
-//   mockEnquiryFromNCM(ncm, world),
-//   mockUpdate("CXU02", {
-//     expectedRequest:
-//       "<FSC>K01YZ</FSC><IDS>K00/410801G BASS                    </IDS><CCR>K97/1626/8395Q                 </CCR><COU>I2576                                                                       BASS/BARRY                                            011020110000</COU><CCH>K001              TH68010 </CCH><ADJ>INOT GUILTY   GUILTY        011020110000 </ADJ><DIS>I1002M12                   00                                                                            </DIS><DIS>I3025Y999                  00            SEA MONKEY                                                      </DIS><CCH>K002              TH68010 </CCH><ADJ>INOT GUILTY   GUILTY        011020110000 </ADJ><DIS>I1002M14                   00                                                                            </DIS><DIS>I3107                      00                                                                            </DIS>"
-//   })
-// ];
+module.exports = () => [
+  {
+    matchRegex: "CXE01",
+    response: `<?XML VERSION="1.0" STANDALONE="YES"?>
+    <CXE01>
+      <GMH>073ENQR000020SENQASIPNCA05A73000017300000120210316152773000001                                             050001772</GMH>
+      <ASI>
+        <FSC>K04CA</FSC>
+        <IDS>K12/14X     AVALON                  </IDS>
+        <CCR>K12/2732/15R                   </CCR>
+        <COF>K001    1:9:7:1      OF61016 01062009                </COF>
+        <CCR>K12/2732/16T                   </CCR>
+        <COF>K001    1:9:7:1      SX03001A01062009                </COF>
+        <COF>K002    1:9:7:1      OF61016 01062009                </COF>
+      </ASI>
+      <GMT>000008073ENQR004540S</GMT>
+    </CXE01>`,
+    expectedRequest: ""
+  },
+  mockUpdate("CXU02")
+];
--- features/717-HO100332/test.feature
+++ features/717-HO100332/test.feature
@@ -9,9 +9,16 @@ Feature: {717} HO100332
     Given the data for this test is in the PNC
       And "input-message" is received
 
-  @NextUI
-  Scenario: Resolving one pair of HO100310 exceptions
+  Scenario: Resolving one pair of HO100332 exceptions
     Given I am logged in as "generalhandler"
       And I view the list of exceptions
-    When I wait 3000 seconds
-    Then I see exception "HO100332" in the exception list table
+    Then I see exception "HO100332 (2)" in the exception list table
+    When I open the record for "AVALON MARTIN"
+      And I click the "Offences" tab
+      And I view offence "1"
+      And I match the offence to PNC offence "1" in case "12/2732/15R"
+      And I return to the offence list
+      And I view offence "2"
+      And I match the offence to PNC offence "2" in case "12/2732/16T"
+      And I submit the record
+    Then the PNC updates the record
