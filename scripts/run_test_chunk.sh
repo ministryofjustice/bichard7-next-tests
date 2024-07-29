b304bb38fa653ad5d55fbd006c4edea957aef4cf
Emad Karamad <emad.karamad@madetech.com>
2024-07-29 14:42:59 +0100

    Add new jobs to run tests against phase 2 core

--- scripts/run_test_chunk.sh
+++ scripts/run_test_chunk.sh
@@ -12,4 +12,10 @@ else
   TAGS="${TAGS} and not @ExcludeOnLegacyUI"
 fi
 
+if [[ "$PHASE2_CORE_CANARY_RATIO" -eq 1 ]]; then
+  TAGS="${TAGS} and @Phase2Core"
+fi
+
+echo "Tags: ${TAGS}"
+
 ./node_modules/.bin/cucumber-js --require steps/index.js --retry 5 --no-strict --exit --publish-quiet --format @cucumber/pretty-formatter  --format junit:./cucumber/results/test-results.xml --tags "${TAGS}" $CHUNK
