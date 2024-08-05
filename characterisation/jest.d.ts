59557551069b947f3cd3b42b6e33ffa2a1b3635b
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-05 11:36:50 +0100

    Add method for checking phase to Jest Describe

--- characterisation/jest.d.ts
+++ characterisation/jest.d.ts
@@ -4,4 +4,9 @@ declare namespace jest {
   interface It {
     ifNewBichard: (...args: any[]) => void
   }
+
+  interface Describe {
+    ifPhase1: (...args: any[]) => void
+    ifPhase2: (...args: any[]) => void
+  }
 }
