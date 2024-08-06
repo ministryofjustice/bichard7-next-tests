e14c3c2c1e3579dbbaa9031b2a9f3f3596d9a8fd
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-06 10:42:49 +0100

    Update HO200108 characterisation tests
    - Add tests for when offence result is judgement with final result.
    - Add tests for when not to create an exception.
    - Rename and restructure fixtures for input messages.

--- characterisation/exceptions/HO200108.test.ts
+++ characterisation/exceptions/HO200108.test.ts
@@ -11,14 +11,32 @@ describe.ifPhase2("HO200108", () => {
   })
 
   it.each([
-    { templateFile: "test-data/AnnotatedHearingOutcome-HO200108.xml.njk", messageType: "AHO" },
-    { templateFile: "test-data/PncUpdateDataset-HO200108.xml.njk", messageType: "PncUpdateDataset" }
-  ])("creates an exception for $messageType", async ({ templateFile }) => {
-    const aho = generateMessage(templateFile, {})
+    {
+      templateFile: "test-data/HO200108/aho-adjournment-with-judgement.xml.njk",
+      messageType: "AHO",
+      result: "adjournment with judgement"
+    },
+    {
+      templateFile: "test-data/HO200108/pud-adjournment-with-judgement.xml.njk",
+      messageType: "PncUpdateDataset",
+      result: "adjournment with judgement"
+    },
+    {
+      templateFile: "test-data/HO200108/aho-judgement-with-final-result.xml.njk",
+      messageType: "AHO",
+      result: "judgement with final result"
+    },
+    {
+      templateFile: "test-data/HO200108/pud-judgement-with-final-result.xml.njk",
+      messageType: "PncUpdateDataset",
+      result: "judgement with final result"
+    }
+  ])("creates a HO200108 exception for $messageType when $result", async ({ templateFile }) => {
+    const inputMessage = generateMessage(templateFile, {})
 
     const {
       outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(aho)
+    } = await processPhase2Message(inputMessage)
 
     expect(exceptions).toStrictEqual([
       {
@@ -26,5 +44,27 @@ describe.ifPhase2("HO200108", () => {
         path: offenceResultClassPath(0, 0)
       }
     ])
+  })
+
+  it.each([
+    {
+      templateFile: "test-data/HO200108/aho-adjournment-with-judgement-not-2060-disposal.xml.njk",
+      result: "adjournment with judgement"
+    },
+    {
+      templateFile: "test-data/HO200108/aho-judgement-with-final-result-recordable-result.xml.njk",
+      result: "judgement with final result"
+    }
+  ])("doesn't create a HO200108 exception when $result", async ({ templateFile }) => {
+    const inputMessage = generateMessage(templateFile, {})
+
+    const {
+      outputMessage: { Exceptions: exceptions }
+    } = await processPhase2Message(inputMessage)
+
+    expect(exceptions).not.toContain({
+      code: "HO200108",
+      path: offenceResultClassPath(0, 0)
+    })
   })
 })
