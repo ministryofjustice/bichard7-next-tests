7dc95a4ccb0d24589c549637bc739b01174d888b
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-06 11:25:54 +0100

    Update HO200101 characterisation tests
    - Add tests for when offence result is judgement with final result.
    - Add tests for when not to create an exception.
    - Rename and restructure fixtures for input messages.

--- characterisation/exceptions/HO200101.test.ts
+++ characterisation/exceptions/HO200101.test.ts
@@ -10,8 +10,8 @@ describe.ifPhase2("HO200101", () => {
     await new World({}).db.closeConnection()
   })
 
-  it("creates an exception for an AHO", async () => {
-    const aho = generateMessage("test-data/AnnotatedHearingOutcome-HO200101.xml.njk", {})
+  it("creates a HO200101 exception for AHO when adjournment with judgement", async () => {
+    const aho = generateMessage("test-data/HO200101/aho-adjournment-with-judgement.xml.njk", {})
 
     const {
       outputMessage: { Exceptions: exceptions }
@@ -25,13 +25,32 @@ describe.ifPhase2("HO200101", () => {
     ])
   })
 
-  it("doesn't create an exception for a PncUpdateDataset", async () => {
-    const pncUpdateDataset = generateMessage("test-data/PncUpdateDataset-HO200101.xml.njk", {})
+  it.each([
+    {
+      templateFile: "test-data/HO200101/aho-judgement-with-final-result.xml.njk",
+      messageType: "AHO",
+      result: "adjournment with judgement"
+    },
+    {
+      templateFile: "test-data/HO200101/pud-adjournment-with-judgement.xml.njk",
+      messageType: "PncUpdateDataset",
+      result: "adjournment with judgement"
+    },
+    {
+      templateFile: "test-data/HO200101/pud-judgement-with-final-result.xml.njk",
+      messageType: "PncUpdateDataset",
+      result: "judgement with final result"
+    }
+  ])("doesn't create a HO200101 exception for $messageType when $result", async ({ templateFile }) => {
+    const inputMessage = generateMessage(templateFile, {})
 
     const {
       outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(pncUpdateDataset)
+    } = await processPhase2Message(inputMessage)
 
-    expect(exceptions).toHaveLength(0)
+    expect(exceptions).not.toContain({
+      code: "HO200101",
+      path: offenceResultClassPath(0, 0)
+    })
   })
 })
