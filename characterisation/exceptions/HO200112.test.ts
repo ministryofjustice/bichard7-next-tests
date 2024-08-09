0e4bdad5a60a2ef43bcfa560985e45cfc73df004
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-09 14:04:09 +0100

    Add HO200112 characterisation tests (#595)

---
+++ characterisation/exceptions/HO200112.test.ts
@@ -0,0 +1,38 @@
+import World from "../../utils/world"
+import { generateMessage } from "../helpers/generateMessage"
+import { processPhase2Message } from "../helpers/processMessage"
+
+jest.setTimeout(30000)
+
+describe.ifPhase2("HO200112", () => {
+  afterAll(async () => {
+    await new World({}).db.closeConnection()
+  })
+
+  it.each([
+    {
+      templateFile: "test-data/HO200112/aho.xml.njk",
+      messageType: "AHO"
+    },
+    {
+      templateFile: "test-data/HO200112/pud.xml.njk",
+      messageType: "PncUpdateDataset"
+    }
+  ])(
+    "creates a HO200112 exception for $messageType when result with judgement with final result and sentence",
+    async ({ templateFile }) => {
+      const inputMessage = generateMessage(templateFile, {})
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      expect(exceptions).toStrictEqual([
+        {
+          code: "HO200112",
+          path: ["AnnotatedHearingOutcome", "HearingOutcome", "Case", "HearingDefendant", "ArrestSummonsNumber"]
+        }
+      ])
+    }
+  )
+})
