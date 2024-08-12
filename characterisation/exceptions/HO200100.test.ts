3ce731847d976883629ee7277f86ef10bbca97fe
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-12 10:41:03 +0100

    Refactor HO200100 characterisation tests

--- characterisation/exceptions/HO200100.test.ts
+++ characterisation/exceptions/HO200100.test.ts
@@ -1,7 +1,9 @@
-import generateMessage from "../helpers/generateMessage"
 import World from "../../utils/world"
 import { processPhase2Message } from "../helpers/processMessage"
 import { offenceResultClassPath } from "../helpers/errorPaths"
+import MessageType from "../types/MessageType"
+import generatePhase2Message from "../helpers/generatePhase2Message"
+import { ResultClass } from "../types/ResultClass"
 
 describe.ifPhase2("HO200100", () => {
   afterAll(async () => {
@@ -9,43 +11,42 @@ describe.ifPhase2("HO200100", () => {
   })
 
   describe("when adjournment pre-judgement for offence result", () => {
-    it.each([
-      {
-        templateFile: "test-data/HO200100/aho-pnc-adjudication-exists.xml.njk",
-        messageType: "AHO"
-      },
-      {
-        templateFile: "test-data/HO200100/pud-pnc-adjudication-exists.xml.njk",
-        messageType: "PncUpdateDataset"
+    it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+      "creates a HO200100 exception for %s when PNC adjudication exists",
+      async (messageType) => {
+        const inputMessage = generatePhase2Message({
+          messageType,
+          offences: [
+            {
+              results: [{ resultClass: ResultClass.ADJOURNMENT_PRE_JUDGEMENT, pncAdjudicationExists: true }]
+            }
+          ]
+        })
+
+        const {
+          outputMessage: { Exceptions: exceptions }
+        } = await processPhase2Message(inputMessage)
+
+        expect(exceptions).toStrictEqual([
+          {
+            code: "HO200100",
+            path: offenceResultClassPath(0, 0)
+          }
+        ])
       }
-    ])("creates a HO200100 exception for $messageType when PNC adjudication exists", async ({ templateFile }) => {
-      const inputMessage = generateMessage(templateFile, {})
+    )
 
-      const {
-        outputMessage: { Exceptions: exceptions }
-      } = await processPhase2Message(inputMessage)
-
-      expect(exceptions).toStrictEqual([
-        {
-          code: "HO200100",
-          path: offenceResultClassPath(0, 0)
-        }
-      ])
-    })
-
-    it.each([
-      {
-        templateFile: "test-data/HO200100/aho-no-pnc-adjudication-exists.xml.njk",
-        messageType: "AHO"
-      },
-      {
-        templateFile: "test-data/HO200100/pud-no-pnc-adjudication-exists.xml.njk",
-        messageType: "PncUpdateDataset"
-      }
-    ])(
-      "doesn't create a HO200100 exception for $messageType when PNC adjudication doesn't exist",
-      async ({ templateFile }) => {
-        const inputMessage = generateMessage(templateFile, {})
+    it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+      "doesn't create a HO200100 exception for %s when PNC adjudication doesn't exist",
+      async (messageType) => {
+        const inputMessage = generatePhase2Message({
+          messageType,
+          offences: [
+            {
+              results: [{ resultClass: ResultClass.ADJOURNMENT_PRE_JUDGEMENT, pncAdjudicationExists: false }]
+            }
+          ]
+        })
 
         const {
           outputMessage: { Exceptions: exceptions }
