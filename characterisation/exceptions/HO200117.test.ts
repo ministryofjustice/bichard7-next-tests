f1ba72c47788a64a6c718559f3f50c6c3cfc9a19
BushraAbdullahi <103956365+BushraAbdullahi@users.noreply.github.com>
2024-08-14 11:05:31 +0100

    Add Characterisation tests for HO200117 (#605)
    * WIP
    
    * write tests for HO200117 and add error path
    
    * edit typo
    
    * remove redundant tests
    
    * update test description
    
    * remove redundant code

---
+++ characterisation/exceptions/HO200117.test.ts
@@ -0,0 +1,38 @@
+import World from "../../utils/world"
+import { processPhase2Message } from "../helpers/processMessage"
+import { offenceReasonPath } from "../helpers/errorPaths"
+import generatePhase2Message from "../helpers/generatePhase2Message"
+import MessageType from "../types/MessageType"
+
+describe.ifPhase2("HO200117", () => {
+  afterAll(async () => {
+    await new World({}).db.closeConnection()
+  })
+
+  it("creates a HO200117 exception when there are more than 10 recordable results", async () => {
+    const recordableResults = Array.from({ length: 11 }, () => ({
+      cjsResultCode: 1015,
+      recordableOnPncIndicator: true
+    }))
+
+    const inputMessage = generatePhase2Message({
+      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
+      offences: [
+        {
+          results: recordableResults
+        }
+      ]
+    })
+
+    const {
+      outputMessage: { Exceptions: exceptions }
+    } = await processPhase2Message(inputMessage)
+
+    expect(exceptions).toStrictEqual([
+      {
+        code: "HO200117",
+        path: offenceReasonPath(0)
+      }
+    ])
+  })
+})
