31123b1cd0fde4dc9240706ada16e4f763593e52
BushraAbdullahi <bushra1175@gmail.com>
2024-08-14 14:39:37 +0100

    update description

--- characterisation/exceptions/HO200117.test.ts
+++ characterisation/exceptions/HO200117.test.ts
@@ -10,7 +10,7 @@ describe.ifPhase2("HO200117", () => {
   })
 
 it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
-  "creates a HO200117 exception when there are more than 10 recordable results",
+  "creates a HO200117 exception for %s when there are more than 10 recordable results",
   async (messageType) => {
     const recordableResults = Array.from({ length: 11 }, () => ({
       cjsResultCode: 1015,
