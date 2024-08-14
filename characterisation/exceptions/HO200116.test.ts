9d6c2336713b6f529be4688fb652c4541656c02d
BushraAbdullahi <bushra1175@gmail.com>
2024-08-14 02:15:22 +0100

    remove redundant tests

--- characterisation/exceptions/HO200116.test.ts
+++ characterisation/exceptions/HO200116.test.ts
@@ -34,37 +34,4 @@ describe.ifPhase2("HO200116", () => {
     ])
   })
 
-  it("does not create a HO200116 exception when there are 100 offences", async () => {
-    const offences = Array.from({ length: 100 }, () => ({
-      results: [{ cjsResultCode: 1015 }]
-    }))
-
-    const inputMessage = generatePhase2Message({
-      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
-      offences: offences
-    })
-
-    const {
-      outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(inputMessage)
-
-    expect(exceptions).toHaveLength(0)
-  })
-
-  it("does not create a HO200116 exception when there are less than 100 offences (there are 99)", async () => {
-    const offences = Array.from({ length: 99 }, () => ({
-      results: [{ cjsResultCode: 1015 }]
-    }))
-
-    const inputMessage = generatePhase2Message({
-      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
-      offences: offences
-    })
-
-    const {
-      outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(inputMessage)
-
-    expect(exceptions).toHaveLength(0)
-  })
 })
