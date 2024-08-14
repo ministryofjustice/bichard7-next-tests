96f593962afdbaa911fd484e36c0d94610f45a62
BushraAbdullahi <bushra1175@gmail.com>
2024-08-14 12:36:03 +0100

    complete test

--- characterisation/exceptions/HO200113.test.ts
+++ characterisation/exceptions/HO200113.test.ts
@@ -1,37 +1,43 @@
 import World from "../../utils/world"
 import { processPhase2Message } from "../helpers/processMessage"
-import { offenceResultClassPath } from "../helpers/errorPaths"
+import { asnPath } from "../helpers/errorPaths"
 import generatePhase2Message from "../helpers/generatePhase2Message"
 import MessageType from "../types/MessageType"
-
-jest.setTimeout(30000)
+import { ResultClass } from "../types/ResultClass"
 
 describe.ifPhase2("HO200113", () => {
   afterAll(async () => {
     await new World({}).db.closeConnection()
   })
 
-  it("creates a HO200113 exception when NEWREM exists, there are no remand CCRs, and SENDEF exists", async () => {
-    const inputMessage = generatePhase2Message({
-      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
-      offences: [
+  it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+    "creates a HO200113 exception when NEWREM exists, there are no remand CCRs, and SENDEF exists",
+    async () => {
+      const inputMessage = generatePhase2Message({
+        messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
+        offences: [
+          {
+            recordableOnPncIndicator: true,
+            results: [
+              { cjsResultCode: 1015 },
+              { cjsResultCode: 1015, resultClass: ResultClass.SENTENCE, pncAdjudicationExists: true }
+            ],
+            addedByTheCourt: true
+          }
+        ]
+      })
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      console.log((await processPhase2Message(inputMessage)).outputMessage)
+      expect(exceptions).toStrictEqual([
         {
-          recordableOnPncIndicator: true,
-          results: [{ cjsResultCode: 1015 }]
+          code: "HO200113",
+          path: asnPath
         }
-      ]
-    })
-
-    const {
-      outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(inputMessage)
-
-    console.log((await processPhase2Message(inputMessage)).outputMessage)
-    expect(exceptions).toStrictEqual([
-      {
-        code: "HO200113",
-        path: offenceResultClassPath(0, 0)
-      }
-    ])
-  })
+      ])
+    }
+  )
 })
