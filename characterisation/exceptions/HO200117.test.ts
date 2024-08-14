97799d791de1c0cda7eb4e305b625752a1e037c6
BushraAbdullahi <bushra1175@gmail.com>
2024-08-14 02:14:29 +0100

    remove redundant tests

--- characterisation/exceptions/HO200117.test.ts
+++ characterisation/exceptions/HO200117.test.ts
@@ -29,7 +29,7 @@ describe.ifPhase2("HO200117", () => {
     const {
       outputMessage: { Exceptions: exceptions }
     } = await processPhase2Message(inputMessage)
-    
+
     expect(exceptions).toStrictEqual([
       {
         code: "HO200117",
@@ -37,50 +37,4 @@ describe.ifPhase2("HO200117", () => {
       }
     ])
   })
-
-  it("does not create a HO200117 exception when there are 10 recordable results", async () => {
-    const recordableResults = Array.from({ length: 10 }, () => ({
-      cjsResultCode: 1015,
-      recordableOnPncIndicator: true
-    }))
-
-    const inputMessage = generatePhase2Message({
-      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
-      offences: [
-        {
-          results: recordableResults
-        }
-      ]
-    })
-
-    const {
-      outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(inputMessage)
-
-    expect(exceptions).toHaveLength(0)
-  })
-
-  it("does not create a HO200117 exception when there are less than 10 recordable results (there are 9)", async () => {
-    const recordableResults = Array.from({ length: 9 }, () => ({
-      cjsResultCode: 1015,
-      recordableOnPncIndicator: true
-    }))
-
-    const inputMessage = generatePhase2Message({
-      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
-      offences: [
-        {
-          results: recordableResults
-        }
-      ]
-    })
-
-    const {
-      outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(inputMessage)
-
-    expect(exceptions).toHaveLength(0)
-  })
 })
-
-
