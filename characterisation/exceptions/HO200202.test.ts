60d484a4c4bbeb9dbc2f5df8b5194ec53d72f539
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-14 16:01:40 +0100

    Add HO200202 characterisation tests (#609)
    * Add resultQualifierVariables to generatePhase2Message
    
    * Add resultQualifierVariableCodePath to errorPaths
    
    * Add HO200202 characterisation tests

---
+++ characterisation/exceptions/HO200202.test.ts
@@ -0,0 +1,59 @@
+import World from "../../utils/world"
+import generatePhase2Message from "../helpers/generatePhase2Message"
+import { processPhase2Message } from "../helpers/processMessage"
+import { resultQualifierVariableCodePath } from "../helpers/errorPaths"
+import MessageType from "../types/MessageType"
+
+describe.ifPhase2("HO200202", () => {
+  afterAll(async () => {
+    await new World({}).db.closeConnection()
+  })
+
+  it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+    "creates a HO200202 exception for %s when too many qualifier variables for results",
+    async (messageType) => {
+      const inputMessage = generatePhase2Message({
+        messageType,
+        offences: [{ results: [{ resultQualifierVariables: [1, 2, 3, 4, 5], pncAdjudicationExists: true }] }],
+        pncId: "2000/0000000X",
+        pncAdjudication: {},
+        pncDisposals: [{ type: 1000 }]
+      })
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      expect(exceptions).toStrictEqual(
+        expect.arrayContaining(
+          Array.from({ length: 5 }, (_, index) => ({
+            code: "HO200202",
+            path: resultQualifierVariableCodePath(0, 0, index)
+          }))
+        )
+      )
+    }
+  )
+
+  it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+    "doesn't create a HO200202 exception for %s when within limit for qualifier variables for results",
+    async (messageType) => {
+      const inputMessage = generatePhase2Message({
+        messageType,
+        offences: [{ results: [{ resultQualifierVariables: [1], pncAdjudicationExists: true }] }],
+        pncId: "2000/0000000X",
+        pncAdjudication: {},
+        pncDisposals: [{ type: 1000 }]
+      })
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      expect(exceptions).not.toContainEqual({
+        code: "HO200202",
+        path: resultQualifierVariableCodePath(0, 0, 0)
+      })
+    }
+  )
+})
