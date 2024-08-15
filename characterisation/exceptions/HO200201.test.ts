e88aec6c3ff2117ac2a26aafc962e57e0a4226c1
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2024-08-15 12:06:53 +0100

    Add HO200201 characterisation tests

---
+++ characterisation/exceptions/HO200201.test.ts
@@ -0,0 +1,73 @@
+import World from "../../utils/world"
+import generatePhase2Message from "../helpers/generatePhase2Message"
+import { processPhase2Message } from "../helpers/processMessage"
+import MessageType from "../types/MessageType"
+import { resultQualifierVariableDurationTypePath } from "../helpers/errorPaths"
+
+describe.ifPhase2("HO200201", () => {
+  afterAll(async () => {
+    await new World({}).db.closeConnection()
+  })
+
+  it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+    "creates a HO200201 exception for %s when qualifier variable for result contains duration",
+    async (messageType) => {
+      const inputMessage = generatePhase2Message({
+        messageType,
+        offences: [
+          {
+            results: [
+              {
+                resultQualifierVariables: [{ code: 1, duration: { type: "Custodial", unit: "D", length: 30 } }],
+                pncAdjudicationExists: true
+              }
+            ]
+          }
+        ],
+        pncId: "2000/0000000X",
+        pncAdjudication: {},
+        pncDisposals: [{ type: 1000 }]
+      })
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      expect(exceptions).toContainEqual({
+        code: "HO200201",
+        path: resultQualifierVariableDurationTypePath(0, 0, 0)
+      })
+    }
+  )
+
+  it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+    "doesn't create a HO200201 exception for %s when qualifier variable for result doesn't contain a duration",
+    async (messageType) => {
+      const inputMessage = generatePhase2Message({
+        messageType,
+        offences: [
+          {
+            results: [
+              {
+                resultQualifierVariables: [{ code: 1 }],
+                pncAdjudicationExists: true
+              }
+            ]
+          }
+        ],
+        pncId: "2000/0000000X",
+        pncAdjudication: {},
+        pncDisposals: [{ type: 1000 }]
+      })
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      expect(exceptions).not.toContainEqual({
+        code: "HO200201",
+        path: resultQualifierVariableDurationTypePath(0, 0, 0)
+      })
+    }
+  )
+})
