51b933d14ebdb222f1932f928bd0092beafb4520
Emad Karamad <emad.karamad@madetech.com>
2024-08-22 13:07:27 +0100

    Characterisation tests - TRPS0002 and TRPS0011 (#617)
    * Add TRPS0002 and TRPS0011 to TriggerCode enum
    
    * Add TRPS0002 and TRPS0011 tests for no ops and exceptions case
    
    * Fix lint issues
    
    * Update Phase2Message.xml.njk for added by the court
    
    * Add tests for TRPS0002 and TRPS0011 when ho is aint case
    
    * Fix offenceReasonSequence in Phase2Message.xml.njk
    
    * Update generatePhase2Message for new Bichard tests
    
    * Cleanup
    
    * Remove code constant variable from TRPS0002 and TRPS0011

--- characterisation/exceptions/HO200117.test.ts
+++ characterisation/exceptions/HO200117.test.ts
@@ -9,34 +9,33 @@ describe.ifPhase2("HO200117", () => {
     await new World({}).db.closeConnection()
   })
 
-it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
-  "creates a HO200117 exception for %s when there are more than 10 recordable results",
-  async (messageType) => {
-    const recordableResults = Array.from({ length: 11 }, () => ({
-      cjsResultCode: 1015,
-      recordableOnPncIndicator: true
-    }))
+  it.each([MessageType.ANNOTATED_HEARING_OUTCOME, MessageType.PNC_UPDATE_DATASET])(
+    "creates a HO200117 exception for %s when there are more than 10 recordable results",
+    async (messageType) => {
+      const recordableResults = Array.from({ length: 11 }, () => ({
+        cjsResultCode: 1015,
+        recordableOnPncIndicator: true
+      }))
 
-    const inputMessage = generatePhase2Message({
-      messageType: messageType,
-      offences: [
+      const inputMessage = generatePhase2Message({
+        messageType: messageType,
+        offences: [
+          {
+            results: recordableResults
+          }
+        ]
+      })
+
+      const {
+        outputMessage: { Exceptions: exceptions }
+      } = await processPhase2Message(inputMessage)
+
+      expect(exceptions).toStrictEqual([
         {
-          results: recordableResults
+          code: "HO200117",
+          path: offenceReasonPath(0)
         }
-      ]
-    })
-
-    const {
-      outputMessage: { Exceptions: exceptions }
-    } = await processPhase2Message(inputMessage)
-
-    expect(exceptions).toStrictEqual([
-      {
-        code: "HO200117",
-        path: offenceReasonPath(0)
-      }
-    ])
-  }
-)
-
+      ])
+    }
+  )
 })
--- characterisation/helpers/generatePhase2Message.ts
+++ characterisation/helpers/generatePhase2Message.ts
@@ -1,6 +1,6 @@
+import MessageType from "../types/MessageType"
+import { ResultClass } from "../types/ResultClass"
 import generateMessage from "./generateMessage"
-import type { ResultClass } from "../types/ResultClass"
-import type MessageType from "../types/MessageType"
 
 type PncDisposal = {
   type: number
@@ -46,16 +46,67 @@ type Offence = {
 
 export type GeneratePhase2MessageOptions = {
   messageType: MessageType
+  hoTemplate?: "NoOperationsAndExceptions" | "AintCase"
   penaltyNoticeCaseReference?: boolean
   recordableOnPncIndicator?: boolean
   arrestSummonsNumber?: string
+  penaltyNoticeCaseReferenceNumber?: string
   offences?: Offence[]
   pncId?: string
   pncAdjudication?: PncAdjudication
   pncDisposals?: PncDisposal[]
 }
 
-const generatePhase2Message = (options: GeneratePhase2MessageOptions) =>
-  generateMessage("test-data/Phase2Message.xml.njk", options)
+const updateOptionsForNoOperationsAndExceptions = (
+  options: GeneratePhase2MessageOptions
+): GeneratePhase2MessageOptions => {
+  if (process.env.USE_BICHARD === "true" && options.messageType === MessageType.PNC_UPDATE_DATASET) {
+    throw Error("This test only works in the new Bichard")
+  }
+
+  options.recordableOnPncIndicator = true
+  if (!options.offences || options.offences.length === 0) {
+    options.offences = [{}]
+  }
+
+  options.offences?.forEach((offence) => {
+    offence.addedByTheCourt = true
+    if (!offence.results || offence.results.length === 0) {
+      offence.results = [{}]
+    }
+
+    offence.results?.forEach((result) => {
+      result.resultClass = ResultClass.SENTENCE
+      result.pncAdjudicationExists = false
+    })
+  })
+
+  return options
+}
+
+const updateOptionsForAintCase = (options: GeneratePhase2MessageOptions): GeneratePhase2MessageOptions => {
+  options.recordableOnPncIndicator = true
+  if (!options.offences || options.offences.length === 0) {
+    options.offences = [{}]
+  }
+
+  const offence = options.offences[0]
+  offence.addedByTheCourt = false
+  offence.results = !offence.results || offence.results.length === 0 ? [{}] : offence.results
+  offence.results[0].pncDisposalType = 1000
+  offence.results[0].resultVariableText = "Hearing on 01/01/2025 confirmed."
+
+  return options
+}
+
+const generatePhase2Message = (options: GeneratePhase2MessageOptions): string => {
+  if (options.hoTemplate === "NoOperationsAndExceptions") {
+    options = updateOptionsForNoOperationsAndExceptions(options)
+  } else if (options.hoTemplate === "AintCase") {
+    options = updateOptionsForAintCase(options)
+  }
+
+  return generateMessage("test-data/Phase2Message.xml.njk", options)
+}
 
 export default generatePhase2Message
