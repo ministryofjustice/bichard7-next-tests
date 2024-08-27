535ea86857a545925d92b37d0911f9fd80940be8
Emad Karamad <emad.karamad@madetech.com>
2024-08-27 16:19:05 +0100

    Add characterisation test for TRPS0013

---
+++ characterisation/triggers/TRPS0013.test.ts
@@ -0,0 +1,47 @@
+import World from "../../utils/world"
+import generatePhase2Message from "../helpers/generatePhase2Message"
+import { processPhase2Message } from "../helpers/processMessage"
+import MessageType from "../types/MessageType"
+import { TriggerCode } from "../types/TriggerCode"
+
+describe.ifPhase2("TRPS0013", () => {
+  afterAll(async () => {
+    await new World({}).db.closeConnection()
+  })
+
+  it("creates a TRPS0013 for AnnotatedHearingOutcome when no operations and exceptions are generated", async () => {
+    const inputMessage = generatePhase2Message({
+      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
+      hoTemplate: "NoOperationsAndExceptions"
+    })
+
+    const { triggers } = await processPhase2Message(inputMessage)
+
+    expect(triggers).toContainEqual({ code: TriggerCode.TRPS0013, offenceSequenceNumber: 3 })
+  })
+
+  it.ifNewBichard(
+    "creates a TRPS0013 for PncUpdateDataset when no operations and exceptions are generated",
+    async () => {
+      const inputMessage = generatePhase2Message({
+        messageType: MessageType.PNC_UPDATE_DATASET,
+        hoTemplate: "NoOperationsAndExceptions"
+      })
+
+      const { triggers } = await processPhase2Message(inputMessage)
+
+      expect(triggers).toContainEqual({ code: TriggerCode.TRPS0013, offenceSequenceNumber: 3 })
+    }
+  )
+
+  it("creates a TRPS0013 for AnnotatedHearingOutcome when hearing outcome is aint case", async () => {
+    const inputMessage = generatePhase2Message({
+      messageType: MessageType.ANNOTATED_HEARING_OUTCOME,
+      hoTemplate: "AintCase"
+    })
+
+    const { triggers } = await processPhase2Message(inputMessage)
+
+    expect(triggers).toContainEqual({ code: TriggerCode.TRPS0013, offenceSequenceNumber: 3 })
+  })
+})
