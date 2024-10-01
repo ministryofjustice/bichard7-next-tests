70051eb5be5246ca47cc818311c90d413e31da57
Ben Pirt <ben@pirt.co.uk>
2024-10-01 15:04:50 +0100

    Remove the cucumber tests now they've been moved into Core

--- features/318-3052-result-no-update-pre-judgement/mock-pnc-responses.ts
+++
@@ -1,5 +0,0 @@
-import { dummyUpdate, mockEnquiryFromNCM } from "../../utils/pncMocks"
-
-import type Bichard from "../../utils/world"
-
-export default (ncm: string, world: Bichard) => [mockEnquiryFromNCM(ncm, world), dummyUpdate]
